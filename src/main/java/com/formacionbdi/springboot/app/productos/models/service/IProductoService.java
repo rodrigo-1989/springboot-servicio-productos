eb863645e65d56cc1f936fcf9f3d4e08cb8eca6f
rodrigo <rodrigo_l321@hotmail.com>
2021-02-24 22:32:39 -0500

    Creando Crud Completo

--- src/main/java/com/formacionbdi/springboot/app/productos/models/service/IProductoService.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/models/service/IProductoService.java
@@ -7,8 +7,7 @@ import java.util.List;
 public interface IProductoService {
 
     public List<Producto> findAll();
-
     public Producto findById(Long id);
-
-
+    public Producto save (Producto producto);
+    public void deleteById(Long id);
 }
